af77672a96c6623653ee3c54c83d1c4219f98d1d
miumerfarooq <umerf3506@gmail.com>
2026-02-01 02:41:02 +0500

    Setup Tailwind CSS, Nuxt Ui and added pages

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
-const message = 'Hello, World!'
+import type { NavigationMenuItem } from '@nuxt/ui';
+
+const items = ref<NavigationMenuItem[]>([
+  {
+    label: 'Home',
+    icon: 'i-lucide-house',
+    to: '/',
+  },
+  {
+    label: 'About',
+    icon: 'i-lucide-book-open',
+    to: '/about'
+  },
+  {
+    label: 'Contact Us',
+    icon: 'i-lucide-contact-round',
+    to: '/contact'
+  }
+])
 </script>
 
 <template>
   <UApp>
-    {{ message }}
+    <!-- Markup shared across all pages, ex: NavBar -->
+    <UNavigationMenu :items="items" />
+    <!-- Markup shared across all pages, ex: NavBar -->
+    <NuxtPage />
   </UApp>
 </template>
